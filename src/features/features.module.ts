39c6c59d82ca494aa988c58ed2681fc977341d28
Gavrilik <oleggavrilik@gmail.com>
2022-03-03 13:47:00 +0300

    create_module

---
+++ src/features/features.module.ts
@@ -0,0 +1,11 @@
+import { Global, Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { AuthorModule } from './author/author.module';
+import { BookModule } from './book/book.module';
+import { UserModule } from './user/user.module';
+
+@Global()
+@Module({
+  imports: [UserModule, BookModule, AuthModule, AuthorModule],
+})
+export class FeaturesModule {}
